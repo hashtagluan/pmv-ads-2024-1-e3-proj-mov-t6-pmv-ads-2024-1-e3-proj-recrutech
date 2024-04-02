8f3f8020c1119c62e5b0205bb21376b953cbeca8
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-02 16:03:29 -0300

    :sparkles: feat: adicionar componente para input de textos

---
+++ frontend/components/TextField.tsx
@@ -0,0 +1,45 @@
+import { Text, View, TextInput, StyleSheet } from "react-native";
+
+import { Colors } from "@/constants/Colors";
+import { TextFieldProps } from "@/types/TextField";
+import { FontSize, Spacing } from "@/constants/Sizes";
+
+export default function TextField({
+  label,
+  variant,
+  secureTextEntry,
+  ...inputProps
+}: TextFieldProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={[styles.input, variant === "secondary" && styles.borderGreen]}
+        {...{ secureTextEntry }}
+        {...inputProps}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    gap: Spacing.extraSmall,
+  },
+  label: {
+    color: Colors.black,
+    fontSize: FontSize.small,
+  },
+  input: {
+    borderWidth: 1,
+    fontSize: FontSize.small,
+    borderColor: Colors.black,
+    paddingHorizontal: Spacing.medium,
+    borderRadius: Spacing.smallMedium,
+    paddingVertical: Spacing.extraSmall,
+  },
+  borderGreen: {
+    borderColor: Colors.green,
+  },
+});
