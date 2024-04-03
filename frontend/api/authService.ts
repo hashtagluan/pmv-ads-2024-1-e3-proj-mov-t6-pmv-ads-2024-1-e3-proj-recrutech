15dda06f1ebae42a9580d9fb0b004647be12cfd5
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-03 03:30:58 -0300

    :sparkles: feat: adicionar serviço para lidar com chamada ao endpoint de login

---
+++ frontend/api/authService.ts
@@ -0,0 +1,25 @@
+import axios, { isAxiosError } from "axios";
+
+import { AuthService } from "@/types/AuthService.interfaces";
+
+export const authService = (): AuthService => {
+  const API_URL = "https://recrutech-webapi.azurewebsites.net/api/Users/Login";
+
+  return {
+    login: async (email: string, password: string) => {
+      try {
+        const response = await axios.post(
+          `${API_URL}?email=${encodeURIComponent(email)}&password=${password}`,
+          {}
+        );
+
+        if (response.data) {
+          console.log("Login successful");
+        }
+      } catch (error) {
+        isAxiosError(error) &&
+          console.error("Error fetching data: ", error.response?.data);
+      }
+    },
+  };
+};
