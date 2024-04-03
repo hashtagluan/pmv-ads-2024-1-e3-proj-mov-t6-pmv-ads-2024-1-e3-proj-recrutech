c3898145142ffe5ac82b00b05f507abca4027bd0
Matheus Rocha <im.rochamatheus@gmail.com>
2024-04-03 20:03:58 -0300

    :sparkles: feat: adicionar diretório e schema para formulário de login

---
+++ frontend/schemas/signupSchema.ts
@@ -0,0 +1,9 @@
+import { FormModel } from "@/types/Login.interfaces";
+import * as yup from "yup";
+
+const loginSchema = yup.object<FormModel>().shape({
+  email: yup.string().email("Email inválido").required("Email obrigatório"),
+  password: yup.string().required("A senha é obrigatória"),
+});
+
+export default loginSchema;
